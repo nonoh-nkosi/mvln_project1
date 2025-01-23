cfa1022ebcf4304464d9d0eaf8d124d0380d3193
ora <segoeoratilee@gmail.com>
2025-01-23 16:53:29 +0200

    adding updated userManagement locators

--- tests/helpers.ts/userManagementLocators.ts
+++ tests/helpers.ts/userManagementLocators.ts
@@ -2,74 +2,86 @@
 
 export const userManagement = {
 
-      //Headings
-      userInformation: 'User Information',
-       heading: 'User Account Management',
-    
-    //Buttons
-    addUserButton: '[id="openModalBtn"]',
-     submitBtn:'button[text()="Submit"]',
-     closeButton: 'span[aria-hidden="true"]',
-    
-     editButton: 'Edit',
-     saveButton: 'button:has-text("Save")',
-     confirmButton: 'button.editConfirmation',
-     signOut: 'span.d-sm-inline',
-     mainPageRole: '[id="optionRoleSelect646"]',
+   //Headings
+   userInformation: 'User Information',
+   userPasswordReset: 'User Password Reset',
+    heading: 'User Account Management',
+ 
+ //Buttons
+ addUserButton: '[id="openModalBtn"]',
+  submitBtn:'button[text()="Submit"]',
+  closeButton: 'span[aria-hidden="true"]',
+ 
+  editButton: 'Edit',
+  saveButton: 'button:has-text("Save")',
+  confirmButton: 'button.editConfirmation',
+  signOut: 'span.d-sm-inline',
+  mainPageRole: '[id="optionRoleSelect646"]',
      confirmBtn: 'button:has-text("Confirm")',
 
-       //singleAccount Role Buttons
-       fillCheckbox: 'label[for="customCheckbox1"].styledCheckbox',
+    //singleAccount Role Buttons
+    fillCheckbox: 'label[for="customCheckbox1"].styledCheckbox',
 
-       bulkAction: '.t-select-btn',
-       bulkDropdown: 'ul.t-dropdown-list',
-      
-       updateUserRole: 'Update User Roles',
+    bulkAction: '.t-select-btn',
+    bulkDropdown: 'ul.t-dropdown-list',
+   
+    updateUserRole: 'Update User Roles',
 
-       bulkActionSelect: '[id="BulkOptionRoleSelect"]',
+    bulkActionSelect: '[id="BulkOptionRoleSelect"]',
 
-       bulkConfirm: '[id="bulkRoleSave"]',
+    bulkConfirm: '[id="bulkRoleSave"]',
 
-       userSelectRole: '[id="optionRoleSelect648"]',
-       //Buttons
-       confirmRole: '[id="confirmRoleBtn"]',
-       cancel: '.btn btn-secondary',
+    userSelectRole: '[id="optionRoleSelect648"]',
+    //Buttons
+    confirmRole: '[id="confirmRoleBtn"]',
+    cancel: '.btn btn-secondary',
 
-       //Confirmation Message
-       resetConfirm: 'Bulk User Roles updated successfully',
+    //Confirmation Message
+    resetConfirm: 'Bulk User Roles updated successfully',
 
 
-    
-    //Fields 
-    nameField: '[id="name"]',
-    surnameField: '[id="surname"]',
-    emailField: '[id="email"]',
-    phoneField: '[id="phone"]',
-    departmentField: '[id="department"]',
-    role:  '[id="role"]',
+   
+ //Fields 
+ nameField: '[id="name"]',
+ surnameField: '[id="surname"]',
+ emailField: '[id="email"]',
+ phoneField: '[id="phone"]',
+ departmentField: '[id="department"]',
+ role:  '[id="role"]',
 
-    //Edit User Fields
-    name: '[id="name_647"]',
-    surname: '[id="surname_647"]',
-    editEmail: '[id="email_647"]',
-    phone: '[id="phone_647"]',
-    department: '[id="department_647"]',
-    editRole:  '[id="role_647"]',
-    email: '[id="email_647"]',
-    
-    //Error messages
-    errorMessage: 'Invalid email address!',
-    editErrorMessage: '.errorDiv',
-    
-    //Success Messages
-    confirmMessage: 'h5:has-text("Please Confirm Changes:")',
-    successMessage: 'text=User information updated successfully',
-    
-    //Links
+ //Edit User Fields
+ name: '[id="name_647"]',
+ surname: '[id="surname_647"]',
+ editEmail: '[id="email_647"]',
+ phone: '[id="phone_647"]',
+ department: '[id="department_647"]',
+ editRole:  '[id="role_647"]',
+ email: '[id="email_647"]',
+
+     //Reset Password Buttons
+     resetPassword1: 'Reset Password',
+     resetPassword2: '.button.btn.btn-secondary',
+ 
+     confirmReset1: 'Send Reset',
+     //or
+     confirmReset2: '.button.btn.btn-secondary',
+ 
+ 
+ //Error messages
+ emailErrorMessage: 'Invalid email address!',
+ editErrorMessage: '.errorDiv',
+ newPasswordErrorMessage: 'Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one special character.',    
+
+ //Success Messages
+ confirmMessage: 'h5:has-text("Please Confirm Changes:")',
+ successMessage: 'text=User information updated successfully',
+ resetPasswordMessage: 'text=User Password reset is successfully sent',
+ 
+ //Links
     userManagement: 'a[href="/User Accounts/userManagement.php"]',
-    userElement: '#fullNameTable_647',
+ userElement: '#fullNameTable_647',
     report: 'a[href="/Reports/reports.php"]',
-    menu: '[id="menuText"]',
+ menu: '[id="menuText"]',
 
 
 }
