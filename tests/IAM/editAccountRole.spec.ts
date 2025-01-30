0c4396ec5c5c0a4813955f8204ec9af769d4435e
MVLN <euphamiam@gmail.com>
2025-01-30 10:48:45 +0200

    Edited edit account roles

--- tests/IAM/editAccountRole.spec.ts
+++ tests/IAM/editAccountRole.spec.ts
@@ -8,8 +8,6 @@ test.describe('Account Roles', () => {
 
     test.beforeEach(async ({ page }) => {
     
-    await login(page);
-    
     await login(page); 
     
     const menu = await page.locator(userManagement.menu);
@@ -22,44 +20,48 @@ test.describe('Account Roles', () => {
     
     const userManagementLink = await page.locator(userManagement.userManagement);
     await userManagementLink.click();
-
 })
 
 })
 
   test('Edit account roles', async ({ page }) => {
 
-    await page.locator(userManagement.cBox1).click();
-    await page.locator(userManagement.cBox2).click();
+    await page.locator(userManagement.fillCheckbox).click();
     await page.click(userManagement.bulkAction);
     
     await page.locator(userManagement.bulkDropdown).getByText(userManagement.updateUserRole).click();
 
     const bulkDropdown = page.locator(userManagement.bulkActionSelect);
-    await bulkDropdown.selectOption(singleRole.role1);
+    await bulkDropdown.selectOption(singleRole.bulkRole1);
 
-    await page.locator(userManagement.bulkConfirm2).click();
-    
+    await page.locator(userManagement.bulkConfirm).click();
   });
   
-  test('Preview account role changes before applying', async ({ page }) => {
+  test('Preview account role and apply changes', async ({ page }) => {
    
-    await page.locator(userManagement.cBox2).click();
+    const dropDown = page.locator(userManagement.userSelectRole);
+    await dropDown.selectOption(singleRole.dropDownRole)
+
+    await page.locator(userManagement.confirmRole).click();
+
+    await expect(dropDown).toHaveValue(singleRole.dropDownRole);
+  })
+  test('Handle Assignment Errors Gracefully', async ({ page }) => {
+
+    await page.locator(userManagement.fillCheckbox).click();
     await page.click(userManagement.bulkAction);
     
-    await page.locator(userManagement.bulkDropdown).getByText(userManagement.updateUserRole2).click();
+    await page.locator(userManagement.bulkDropdown).getByText(userManagement.updateUserRole).click();
 
-    const bulkDropdown = page.locator(userManagement.bulkActionSelect2);
-    await bulkDropdown.selectOption(singleRole.role1);
-    await page.locator(userManagement.bulkConfirm2).click();
+    const bulkDropdown = await page.locator(userManagement.bulkActionSelect);
+    await bulkDropdown.selectOption(singleRole.bulkRole1);
 
-  })
+    //Button does not work, this is related to DN-79.
+    await page.locator(userManagement.cancel).click();
+  });
 
   test.afterEach(async ({ page }) => {
 
-    //await page.locator(landingPage.dashBoardPage.signOut).click();
- 
+    await page.locator(dashBoardPage.signOut).click();
     await page.close(); 
-
-  })
-  
+  });
