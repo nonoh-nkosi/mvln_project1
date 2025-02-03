04d8f930209e8f801f63b26fa130447f140d656f
MVLN <euphamiam@gmail.com>
2025-02-03 23:38:52 +0200

    Added a comment

--- tests/IAM/deactivateUserAccount.spec.ts
+++ tests/IAM/deactivateUserAccount.spec.ts
@@ -2,6 +2,7 @@ import { expect, test } from "@playwright/test";
 import { login } from "../helpers.ts/login.ts";
 import { userManagement } from "../helpers.ts/userManagementLocators.ts";
 import { dashBoardPage } from "../helpers.ts/dashboardpageLocators.ts";
+import { signInPage } from "../helpers.ts/signInPageLocators.ts";
 
 test.describe('Deactivating user account', () => {
 
@@ -11,9 +12,12 @@ await login(page);
 
 const menu = await page.locator(userManagement.menu);
 await menu.click();
+
 const reportsLink = page.locator(userManagement.report);
 await reportsLink.click();
+
 await menu.click();
+
 const userManagementLink = await page.locator(userManagement.userManagement);
 await userManagementLink.click();
 })
@@ -45,6 +49,12 @@ await page.locator(userManagement.signOut).click();
 await login(page);
 
 await expect(page).toHaveURL('http://10.10.10.118/Login%20Pages/sign-in.php');
+
+//Error message for locked account
+const lockedAccountError = await page.locator(signInPage.lockedAccountError);
+await expect(lockedAccountError).toContainText(signInPage.lockedAccountMessage);
+await expect(lockedAccountError).toBeVisible();
+
 });
 
 test.afterEach( async ({ page }) => {
